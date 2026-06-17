94f4a3ee449ed4af42838d53569b879e1267aeb4
Tomáš Lavička <8569210+EmmetBrickHacker@users.noreply.github.com>
2026-06-18 00:37:26 +0200

    Move sample code from main.ts to test.ts
    Remove test/sample code from main.ts and add it to test.ts so the button handlers and ValPacker usage are kept in the test file (which is not compiled into the extension). This moves the input handlers and the test packing/unpacking (let test = 0; test = ValPacker.pack(1, 2)) out of the main runtime.

--- main.ts
+++
@@ -1,8 +0,0 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(ValPacker.unpack(Package.Left, test))
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(ValPacker.unpack(Package.Right, test))
-})
-let test = 0
-test = ValPacker.pack(1, 2)
--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(ValPacker.unpack(Package.Left, test))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(ValPacker.unpack(Package.Right, test))
+})
+let test = 0
+test = ValPacker.pack(1, 2)
